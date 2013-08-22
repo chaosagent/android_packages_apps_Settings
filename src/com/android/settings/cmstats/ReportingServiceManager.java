f05c6e86977fdf9519ee70c76367d33b1717f124
MNT-Mantas <mnt@rootbox.ca>
2013-08-22 16:31:40 +0300

    Build error fix

--- src/com/android/settings/cmstats/ReportingServiceManager.java
+++ src/com/android/settings/cmstats/ReportingServiceManager.java
@@ -28,6 +28,8 @@ import android.net.NetworkInfo;
 import android.util.Log;
 
 public class ReportingServiceManager extends BroadcastReceiver {
+    private static final long MILLIS_PER_HOUR = 60L * 60L * 1000L;
+    private static final long MILLIS_PER_DAY = 24L * MILLIS_PER_HOUR;
     private static final long UPDATE_INTERVAL = 60L * 60L * 1000L;
 
     @Override
