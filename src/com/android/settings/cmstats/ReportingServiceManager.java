c0e2d8ff393867b5742eadd5ac4338271b7b46e8
MNT-Mantas <mnt@rootbox.ca>
2013-08-22 16:44:41 +0300

    Less frequent stats

--- src/com/android/settings/cmstats/ReportingServiceManager.java
+++ src/com/android/settings/cmstats/ReportingServiceManager.java
@@ -29,8 +29,7 @@ import android.util.Log;
 
 public class ReportingServiceManager extends BroadcastReceiver {
     private static final long MILLIS_PER_HOUR = 60L * 60L * 1000L;
-    private static final long MILLIS_PER_DAY = 24L * MILLIS_PER_HOUR;
-    private static final long UPDATE_INTERVAL = 60L * 60L * 1000L;
+    private static final long UPDATE_INTERVAL = 2L * MILLIS_PER_HOUR;
 
     @Override
     public void onReceive(Context context, Intent intent) {
